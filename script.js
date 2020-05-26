125ed2449bac43d6af7e29166c62a8b8dc205d58
Marco <Prosperi>
2020-05-26 17:19:35 +0200

    esercizio completo, mancano le condizioni negative
    ossia se scrivi spazio o numero, risultato errore

---
+++ script.js
@@ -0,0 +1,14 @@
+//variabili
+  var cognomi = ['Verratti', 'Insigne', 'Immobile', 'Balzano', 'Sansovini'];
+  var cognomeUser = "";
+
+// ciclo
+while (!isNaN(cognomeUser)) {
+  cognomeUser = prompt('qual è il tuo cognome?');  //richiesta Cognome
+  var cognomeUserUpper = cognomeUser.toUpperCase();//nome in lista in maiuscolo
+  cognomi.push(cognomeUserUpper);// inivio cognome all'array
+  cognomi.sort();//ordine alfabetico
+  var risultato = cognomi; //variabile da stampare
+  document.getElementById('lista').innerHTML = '<li>' + risultato + '</li>';//stampa su schermo
+  document.getElementById('posizione').innerHTML = 'il tuo cognome è il numero ' + (cognomi.indexOf(cognomeUserUpper) + 1);//stampa su schermo
+}
